812f3ca50c96ece7af268962d69c7d82dbceba70
Seimizu Joukan <joukan.seimizu@gmail.com>
2022-07-02 19:47:34 +0900

    Fix runtime verifier error on X86.
    	$sudo ./target/release/funccount do_sys_open -d 3
    	WARN  : libbpf: load bpf program failed: Permission denied
    	WARN  : libbpf: -- BEGIN DUMP LOG ---
    	WARN  : libbpf:
    	Validating do_stack_trace() func#1...
    	; int do_stack_trace(struct pt_regs *ctx) {
    	29: (bf) r6 = r1
    	30: (b7) r7 = 1
    	; u64 *val, one = 1;
    	31: (7b) *(u64 *)(r10 -40) = r7
    	; int pid = bpf_get_current_pid_tgid() >> 32;
    	32: (85) call bpf_get_current_pid_tgid#14
    	33: (b7) r1 = 0
    	; u64 ts = 0;
    	34: (7b) *(u64 *)(r10 -48) = r1
    	; int pid = bpf_get_current_pid_tgid() >> 32;
    	35: (77) r0 >>= 32
    	; if (pid == self_pid)
    	36: (18) r1 = 0xffffaa91c0858038
    	38: (61) r1 = *(u32 *)(r1 +0)
    	 R0_w=invP(id=0,umax_value=4294967295,var_off=(0x0; 0xffffffff)) R1_w=map_value(id=0,off=56,ks=4,vs=68,imm=0) R6_w=mem_or_null(id=2,ref_obj_id=0,off=0,imm=0) R7_w=invP1 R10=fp0 fp-40_w=invP1 fp-48_w=00000000
    	; if (pid == self_pid)
    	39: (1d) if r1 == r0 goto pc+66
    	 R0_w=invP(id=0,umax_value=4294967295,var_off=(0x0; 0xffffffff)) R1_w=invP(id=0,umax_value=4294967295,var_off=(0x0; 0xffffffff)) R6_w=mem_or_null(id=2,ref_obj_id=0,off=0,imm=0) R7_w=invP1 R10=fp0 fp-40_w=invP1 fp-48_w=00000000
    	; if (target_pid > 0 && pid != target_pid)
    	40: (18) r1 = 0xffffaa91c085803c
    	42: (61) r1 = *(u32 *)(r1 +0)
    	 R0_w=invP(id=0,umax_value=4294967295,var_off=(0x0; 0xffffffff)) R1_w=map_value(id=0,off=60,ks=4,vs=68,imm=0) R6_w=mem_or_null(id=2,ref_obj_id=0,off=0,imm=0) R7_w=invP1 R10=fp0 fp-40_w=invP1 fp-48_w=00000000
    	43: (67) r1 <<= 32
    	44: (c7) r1 s>>= 32
    	; if (target_pid > 0 && pid != target_pid)
    	45: (6d) if r7 s> r1 goto pc+3
    	 R0=invP(id=0,umax_value=4294967295,var_off=(0x0; 0xffffffff)) R1=invP(id=0,umin_value=1,umax_value=2147483647,var_off=(0x0; 0x7fffffff)) R6=mem_or_null(id=2,ref_obj_id=0,off=0,imm=0) R7=invP1 R10=fp0 fp-40=invP1 fp-48=00000000
    	46: (67) r1 <<= 32
    	47: (77) r1 >>= 32
    	48: (5d) if r1 != r0 goto pc+57
    	 R0=invP(id=0,umin_value=1,umax_value=2147483647,var_off=(0x0; 0x7fffffff),s32_min_value=0) R1_w=invP(id=0,umin_value=1,umax_value=2147483647,var_off=(0x0; 0x7fffffff),s32_min_value=0) R6=mem_or_null(id=2,ref_obj_id=0,off=0,imm=0) R7=invP1 R10=fp0 fp-40=invP1 fp-48=00000000
    	; if (trace_type == 1) {
    	49: (18) r1 = 0xffffaa91c0858040
    	51: (61) r1 = *(u32 *)(r1 +0)
    	 R0=invP(id=0,umin_value=1,umax_value=2147483647,var_off=(0x0; 0x7fffffff),s32_min_value=0) R1_w=map_value(id=0,off=64,ks=4,vs=68,imm=0) R6=mem_or_null(id=2,ref_obj_id=0,off=0,imm=0) R7=invP1 R10=fp0 fp-40=invP1 fp-48=00000000
    	; if (trace_type == 1) {
    	52: (55) if r1 != 0x1 goto pc+19
    
    	from 52 to 72: R0=invP(id=0,umin_value=1,umax_value=2147483647,var_off=(0x0; 0x7fffffff),s32_min_value=0) R1_w=invP(id=0,umax_value=4294967295,var_off=(0x0; 0xffffffff)) R6=mem_or_null(id=2,ref_obj_id=0,off=0,imm=0) R7=invP1 R10=fp0 fp-40=invP1 fp-48=00000000
    	; key.pid = pid;
    	72: (63) *(u32 *)(r10 -32) = r0
    	; bpf_get_current_comm(&key.comm, sizeof(key.comm));
    	73: (bf) r1 = r10
    	74: (07) r1 += -28
    	; bpf_get_current_comm(&key.comm, sizeof(key.comm));
    	75: (b7) r2 = 16
    	76: (85) call bpf_get_current_comm#16
    	; key.kstack = bpf_get_stackid(ctx, &stackmap, 0 | BPF_F_FAST_STACK_CMP);
    	77: (bf) r1 = r6
    	78: (18) r2 = 0xffffaa91c4ffa000
    	80: (b7) r3 = 512
    	81: (85) call bpf_get_stackid#27
    	R1 type=mem_or_null expected=ctx
    	processed 50 insns (limit 1000000) max_states_per_insn 0 total_states 4 peak_states 4 mark_read 2
    	WARN  : libbpf: -- END LOG --
    	WARN  : libbpf: failed to load program 'stacktrace_tp'
    	WARN  : libbpf: failed to load object 'funccount_bpf'
    	WARN  : libbpf: failed to load BPF skeleton 'funccount_bpf': -4007
    	Error: Failed to load bpf.

--- funccount/bpf/funccount.bpf.c
+++ funccount/bpf/funccount.bpf.c
@@ -30,8 +30,8 @@ struct exectrace_event {
 struct {
 	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
 	__uint(max_entries, 8192);
-	__type(key, int);
-	__type(value, int);
+	__uint(key_size, sizeof(int));
+	__uint(value_size, sizeof(int));
 } exectrace_pb SEC(".maps");
 
 struct stacktrace_event _stacktrace_event = {};
@@ -64,8 +64,40 @@ int self_pid = 0;
 int target_pid = 0;
 int trace_type = 0;
 
-int do_trace(void *ctx) {
+int do_stack_trace(struct pt_regs *ctx) {
 	struct stacktrace_event key;
+	u64 *val, one = 1;
+	int pid = bpf_get_current_pid_tgid() >> 32;
+	u64 ts = 0;
+
+	if (pid == self_pid)
+		return 0;
+
+	if (target_pid > 0 && pid != target_pid)
+		return 0;
+
+	if (trace_type == 1) 
+		return 0;
+
+	key.pid = pid;
+	bpf_get_current_comm(&key.comm, sizeof(key.comm));
+	key.kstack = bpf_get_stackid(ctx, &stackmap, 0 | BPF_F_FAST_STACK_CMP);
+	key.ustack = bpf_get_stackid(ctx, &stackmap, 0 | BPF_F_FAST_STACK_CMP | BPF_F_USER_STACK);
+	if ((int)key.kstack < 0 && (int)key.ustack < 0) {
+		return 0;
+	}
+
+	val = bpf_map_lookup_elem(&stackcnt, &key);
+	if (val) {
+		(*val)++;
+	} else {
+		bpf_map_update_elem(&stackcnt, &key, &one, BPF_NOEXIST);
+	}
+
+	return 0;
+}
+
+int do_exec_trace(struct pt_regs *ctx) {
 	struct exectrace_event ekey;
 	u64 *val, one = 1;
 	int pid = bpf_get_current_pid_tgid() >> 32;
@@ -77,32 +109,31 @@ int do_trace(void *ctx) {
 	if (target_pid > 0 && pid != target_pid)
 		return 0;
 
-	if (trace_type == 1) {
-		ekey.pid = pid;
-		bpf_get_current_comm(&ekey.comm, sizeof(ekey.comm));
-		ts = bpf_ktime_get_ns();
+	if (trace_type == 0) 
+		return 0;
 
-		ekey.ts0 = ts;
-		ekey.ts1 = ts >> 32;
+	ekey.pid = pid;
+	bpf_get_current_comm(&ekey.comm, sizeof(ekey.comm));
+	ts = bpf_ktime_get_ns();
 
-		bpf_map_update_elem(&exectime, &ekey, &ts, BPF_NOEXIST);
-	} else {
-		key.pid = pid;
-		bpf_get_current_comm(&key.comm, sizeof(key.comm));
-		key.kstack = bpf_get_stackid(ctx, &stackmap, 0 | BPF_F_FAST_STACK_CMP);
-		key.ustack = bpf_get_stackid(ctx, &stackmap, 0 | BPF_F_FAST_STACK_CMP | BPF_F_USER_STACK);
-		if ((int)key.kstack < 0 && (int)key.ustack < 0) {
-			return 0;
-		}
+	ekey.ts0 = ts;
+	ekey.ts1 = ts >> 32;
 
-		val = bpf_map_lookup_elem(&stackcnt, &key);
-		if (val) {
-			(*val)++;
-		} else {
-			bpf_map_update_elem(&stackcnt, &key, &one, BPF_NOEXIST);
-		}
-	}
+	bpf_map_update_elem(&exectime, &ekey, &ts, BPF_NOEXIST);
 
+	return 0;
+}
+
+int do_pid_trace(void *ctx) {
+	int pid = bpf_get_current_pid_tgid() >> 32;
+
+	if (pid == self_pid)
+		return 0;
+
+	if (target_pid > 0 && pid != target_pid)
+		return 0;
+
+	
 	bpf_perf_event_output(ctx,
 			&exectrace_pb,
 			BPF_F_CURRENT_CPU,
@@ -115,17 +146,23 @@ int do_trace(void *ctx) {
 SEC("tp/")
 int stacktrace_tp(void *ctx)
 {
-	do_trace(ctx);
+	do_stack_trace(ctx);
+	do_exec_trace(ctx);
+	do_pid_trace(ctx);
 }
 
 SEC("kprobe/")
 int stacktrace_kb(void *ctx)
 {
-	do_trace(ctx);
+	do_stack_trace(ctx);
+	do_exec_trace(ctx);
+	do_pid_trace(ctx);
 }
 
 SEC("uprobe/")
 int stacktrace_ub(void *ctx)
 {
-	do_trace(ctx);
+	do_stack_trace(ctx);
+	do_exec_trace(ctx);
+	do_pid_trace(ctx);
 }
